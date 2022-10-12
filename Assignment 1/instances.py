00719affc489291a7a7a776c01d5ec444fdea2f8
AleksStijelja <acostijelja@hotmail.com>
2022-10-12 15:36:53 -0400

    added bash script for userdata on run instance

--- Assignment 1/instances.py
+++ Assignment 1/instances.py
@@ -3,6 +3,26 @@ def create_m4large_cluster(client, keyPair, securityGroup):
     lowercase_a = 97
     ids = []
     
+    USERDATA_SCRIPT = '''#!/bin/bash \n mkdir Aleks \n
+    # Install necessary files and run Flask in the back ground 
+
+    sudo apt update
+    mkdir flask_application && cd flask_application
+    sudo apt install python3-pip python3-flask -y
+
+    INSTANCE_NAME=$(ec2metadata --instance-id)
+
+    echo "from flask import Flask
+    app = Flask(__name__)
+    @app.route('/')
+    def hello():
+        return 'VM name: $INSTANCE_NAME'
+    if __name__ == '__main__':
+        app.run(host='0.0.0.0', port=80, debug=True)" > my_app.py
+
+    nohup sudo python3 my_app.py &
+    '''
+    
     for instance in range(5):
         response = client.run_instances(
             BlockDeviceMappings=[
@@ -18,6 +38,7 @@ def create_m4large_cluster(client, keyPair, securityGroup):
             ImageId='ami-08c40ec9ead489470',
             InstanceType='m4.large',
             KeyName=keyPair,
+            UserData=USERDATA_SCRIPT,
             Placement={
                 'AvailabilityZone': 'us-east-1'+chr(lowercase_a + instance),
             },
@@ -47,6 +68,26 @@ def create_t2large_cluster(client, keyPair, securityGroup):
     print('Creating 4 instances of t2.large...')
     lowercase_a = 97
     ids = []
+    
+    USERDATA_SCRIPT = '''#!/bin/bash \n mkdir Aleks \n
+    # Install necessary files and run Flask in the back ground 
+
+    sudo apt update
+    mkdir flask_application && cd flask_application
+    sudo apt install python3-pip python3-flask -y
+
+    INSTANCE_NAME=$(ec2metadata --instance-id)
+
+    echo "from flask import Flask
+    app = Flask(__name__)
+    @app.route('/')
+    def hello():
+        return 'VM name: $INSTANCE_NAME'
+    if __name__ == '__main__':
+        app.run(host='0.0.0.0', port=80, debug=True)" > my_app.py
+
+    nohup sudo python3 my_app.py &
+    '''
 
     for instance in range(4):
         response = client.run_instances(
@@ -63,6 +104,7 @@ def create_t2large_cluster(client, keyPair, securityGroup):
             ImageId='ami-08c40ec9ead489470',
             InstanceType='t2.large',
             KeyName=keyPair,
+            UserData=USERDATA_SCRIPT,
             Placement={
                 'AvailabilityZone': 'us-east-1'+chr(lowercase_a + instance),
             },
