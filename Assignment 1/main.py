a9da93986d5fccb7a0008eb129f8d79cbd0e0e15
AleksStijelja <acostijelja@hotmail.com>
2022-10-15 22:50:12 -0400

    tweaked flask deployement script and added check on main for temporary manual flask deployement

--- Assignment 1/main.py
+++ Assignment 1/main.py
@@ -8,9 +8,9 @@ def main():
     
     #initialise the attributs and clients that will be used
     #The follow 3 can change based on who wants to run (the access token is under me: Aleks)
-    aws_access_key_id = 'ASIAQJSMAGZDYUYHJGVO'
-    secret_access_key = 'IPHMQaRivxClUMXsoPS3X3/ZEsG5t91m8DNavPU7'
-    session_token = 'FwoGZXIvYXdzEML//////////wEaDCIQ/10Lk/+sS0mlKyLEAW+O9tsOSwNlOBgTGJxBCDmVR+h9UiIx4p2/Zo76EOH7dIgXrQ5XdQjbwWRFF1r/VQaYQ3i57ZRuLZ6y9fpJfOVFMA7JijyrSDwV9BHEAzmFIJFXe+4RBDey8cQglb1rJgTEjBmBAbYvvS7xTcvVCe9I2e1/rHG8VpIrnx9ETLw9uDkSXtOpcDtPbpiLYj2js7i3lgRGL6mGucUnDRbPgJRxNjlkKzBDm9EL2Aw371p0Qb9kl/Bp42HqK3YkZ3+ipMG90e8ow8KWmgYyLYhHM6uEfdQ1Ha+LIRvTUGIufr4NYRsRSbV4MjDL25DFjYBS43pCuAku8cX20w=='
+    aws_access_key_id = 'ASIAQJSMAGZD5IT6WWIL'
+    secret_access_key = 'GQIdozQzyVMYpny0rEDSv2wilb+5UNCTFMi/Q5nD'
+    session_token = 'FwoGZXIvYXdzECoaDE7FZRl0YWolFR5qACLEAX9g9aapqFO3l9XNfxXTjK+totnAqtncOEP18ybZC6tpOME5uhAHXqyqitz2QUUXKXRq9DhGlF2LoBDurPaGeYrdUqw5ppHzoJxwEE0qe/qYROVOG5MPHM3tO1Z4P7N4gJC6qFler8MApJu2AzhC9UtUxxKN+8QXKXuKFN51SHVh+AkSUb2TO+xTwc35zqU1QlseHzAV3+DR/NMXjwW1IR18VJYAF8DHFsBlw+nC09Grh0N+b703pOQ8z+enE2YgU6Uixm0ol6atmgYyLX94lWkGC+LRyj+7hJ3VYhOs1T1LdqH0hUWg1Ux74AsQioTaKd9hWL82qeuUDA=='
     
     
     #Keypair for access and security group that needs to be assigned when instance is made (currently under me: Aleks)
@@ -47,42 +47,51 @@ def main():
     print("Sleeping for 60 sec for instances to initialise")
     time.sleep(60)
     
+    continue_run = False
+    while continue_run == False:
+        answer = input("Enter 'yes' when you have manually inputed the flask app in all instances") 
+        if answer == "yes":
+            #get security group id
+            sg_response = EC2.describe_security_groups(
+            GroupNames=[securityGroup]
+            )
+            sg_id = sg_response.get('SecurityGroups')[0].get('GroupId')
+            
+            #get subnets from availability zone a to e
+            all_subnets = EC2.describe_subnets()
+            subnets = []
+            five_zones = ['us-east-1a', 'us-east-1b', 'us-east-1c', 'us-east-1d', 'us-east-1e']
+            for subnet in all_subnets['Subnets']:
+                if subnet['AvailabilityZone'] in five_zones:
+                    subnets.append(subnet['SubnetId'])
+            
+            #Create ELB 
+            load_balancer = create_load_balancer(ELB, subnets, sg_id)
+            print("ELB has been made. URL -> " + str(load_balancer.get('LoadBalancers')[0].get('DNSName')))
+            
+            #get vpc_id
+            vpc_id = EC2.describe_vpcs().get('Vpcs', [{}])[0].get('VpcId', '')
+            
+            #create target groups for cluster 1 and cluster 2
+            tg_cluster1, tg_cluster2 = create_target_groups(ELB, vpc_id)
+            
+            #create listener
+            listener = create_elb_listener(ELB, tg_cluster1, load_balancer)
+            
+            #create rules for listener on cluster1 and cluster2
+            create_rules(ELB, listener, tg_cluster1, tg_cluster2)
+            
+            #Register 5 m4.large instances to target group 1 and 4 t2.large instances to target group 2
+            register_instances_to_target_groups(ELB, m4Large_cluster_ids, t2Large_cluster_ids, tg_cluster1, tg_cluster2 )
+            
+            #Destroy everything when user write 'yes'
+            destructor(EC2, ELB, m4Large_cluster_ids, t2Large_cluster_ids, load_balancer, listener, tg_cluster1, tg_cluster2)
+            
+            continue_run = True
+        else: 
+            continue 
     
-    #get security group id
-    sg_response = EC2.describe_security_groups(
-    GroupNames=[securityGroup]
-    )
-    sg_id = sg_response.get('SecurityGroups')[0].get('GroupId')
     
-    #get subnets from availability zone a to e
-    all_subnets = EC2.describe_subnets()
-    subnets = []
-    five_zones = ['us-east-1a', 'us-east-1b', 'us-east-1c', 'us-east-1d', 'us-east-1e']
-    for subnet in all_subnets['Subnets']:
-        if subnet['AvailabilityZone'] in five_zones:
-            subnets.append(subnet['SubnetId'])
-    
-    #Create ELB 
-    load_balancer = create_load_balancer(ELB, subnets, sg_id)
-    print("ELB has been made. URL -> " + str(load_balancer.get('LoadBalancers')[0].get('DNSName')))
-    
-    #get vpc_id
-    vpc_id = EC2.describe_vpcs().get('Vpcs', [{}])[0].get('VpcId', '')
-    
-    #create target groups for cluster 1 and cluster 2
-    tg_cluster1, tg_cluster2 = create_target_groups(ELB, vpc_id)
-    
-    #create listener
-    listener = create_elb_listener(ELB, tg_cluster1, load_balancer)
-    
-    #create rules for listener on cluster1 and cluster2
-    create_rules(ELB, listener, tg_cluster1, tg_cluster2)
-    
-    #Register 5 m4.large instances to target group 1 and 4 t2.large instances to target group 2
-    register_instances_to_target_groups(ELB, m4Large_cluster_ids, t2Large_cluster_ids, tg_cluster1, tg_cluster2 )
-    
-    #Destroy everything when user write 'yes'
-    destructor(EC2, ELB, m4Large_cluster_ids, t2Large_cluster_ids, load_balancer, listener, tg_cluster1, tg_cluster2)
     
     
     
