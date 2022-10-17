a6d0af4c3a21b0e88ddcc4b47f44d1d431076205
AleksStijelja <acostijelja@hotmail.com>
2022-10-16 23:07:16 -0400

    added cloudwatch client and metrics on analysis

--- Assignment 1/analysis.py
+++ Assignment 1/analysis.py
@@ -1,22 +1,19 @@
-import boto3
 from datetime import datetime, timedelta
 
-def main():
-    # For conect to aws we need some key to set. It can be found in AWS first page >> AWS detail >>‌ AWS CLI.
-    aws_access_key_id = 'ASIAQJSMAGZDT52JX4MM'
-    secret_access_key = 'dVq3C7vYNcHs6gwsWfUREkU+YMAB8Mms1cQXUvJs'
-    session_token = 'FwoGZXIvYXdzED8aDLONnpYzhG7cywXQjSLEAS5IPHCLXfBUPX3BhWOw0bQ/BoBaSYDK4voOBvNG1bU9AF7Ra5niTOxP2WeZ1gMDWU3zFAfJM3IKUTXdARNI8OQYaFwyhXV+t3lmw0ZJKoA+KDqAi+rewK27CAI2ZyEAkNQSIk1pSZ7av5nGgo8VH7P9UL6Zlb5OpvlLLylYqxLLXzIJH/iFNJHq5beJht4k6EaureKn5LnZZbpP+8ZRVFL/yM8246JtGCLxceHgGrwTGB0roqHYOpSClZ6+D2dl8LmuOdwo3OmxmgYyLfZxCu66DT7R8bOOakkAA0eRPH97gJW79xE6rlJuFqr6GfzFeeWkneN23reerg=='
-    client = boto3.client('cloudwatch',
-    aws_access_key_id=aws_access_key_id,
-    aws_secret_access_key=secret_access_key,
-    aws_session_token=session_token,
-    region_name= 'us-east-1'
-    )
+def analysis(cw_client, c1_ids, c2_ids, tgc1, tgc2):
 
-    # Here we capture the cpu_utilization for one of the instances by cpu_utilization 
-    for id in ['i-0432eec2a235d3b4b']:
-        cpu_utilization = client.get_metric_statistics(
-        Period=300,
+    results = open("results.txt", "w")
+
+    results.write("ANALYSIS \n")
+
+    #CPU UTILISATION ON ALL CPUS
+
+    results.write('CPU utilisation on all 9 instance:\n')
+    results.write('-----------------------------------------------------------\n')
+    results.write('CPU utilisation on the instance from cluster1:\n')
+    for id in c1_ids:
+        response = cw_client.get_metric_statistics(
+        Period=60,
         StartTime=datetime.utcnow() - timedelta(minutes=60),
         EndTime=datetime.utcnow(),
         MetricName='CPUUtilization',
@@ -24,7 +21,260 @@ def main():
         Statistics=['Average'],
         Dimensions=[{'Name':'InstanceId', 'Value':id}]
         )
-        print(f"CPU Utilization for machine: {id}")
-        print(f"Average: {cpu_utilization}\n")
+        results.write(f"\nCPU Utilization for machine: {id}\n")
+        for datapoint in response['Datapoints']:
+            results.write(f"{datapoint['Timestamp'].astimezone()} --> {datapoint['Average']}\n")
 
-main()
+    results.write('\nCPU utilisation on the instance from cluster2:\n')
+    for id in c2_ids:
+        response = cw_client.get_metric_statistics(
+        Period=60,
+        StartTime=datetime.utcnow() - timedelta(minutes=60),
+        EndTime=datetime.utcnow(),
+        MetricName='CPUUtilization',
+        Namespace='AWS/EC2',
+        Statistics=['Average'],
+        Dimensions=[{'Name':'InstanceId', 'Value':id}]
+        )
+        results.write(f"\nCPU Utilization for machine: {id}\n")
+        for datapoint in response['Datapoints']:
+            results.write(f"{datapoint['Timestamp'].astimezone()} --> {datapoint['Average']}\n")
+
+    results.write('-----------------------------------------------------------\n')
+
+    #NETWORK PACKETS RECEIVED ON ALL CPUS
+
+    results.write('Network Packets Received on all 9 instance:\n')
+    results.write('-----------------------------------------------------------\n')
+    results.write('Network Packets Received on the instance from cluster1:\n')
+    for id in c1_ids:
+        response = cw_client.get_metric_statistics(
+        Period=60,
+        StartTime=datetime.utcnow() - timedelta(minutes=60),
+        EndTime=datetime.utcnow(),
+        MetricName='NetworkPacketsIn',
+        Namespace='AWS/EC2',
+        Statistics=['Average'],
+        Dimensions=[{'Name':'InstanceId', 'Value':id}]
+        )
+        results.write(f"\nNetwork Packets Received for machine: {id}\n")
+        for datapoint in response['Datapoints']:
+            results.write(f"{datapoint['Timestamp'].astimezone()} --> {datapoint['Average']}\n")
+
+    results.write('\nNetwork Packets Received on the instance from cluster2:\n')
+    for id in c2_ids:
+        response = cw_client.get_metric_statistics(
+        Period=60,
+        StartTime=datetime.utcnow() - timedelta(minutes=60),
+        EndTime=datetime.utcnow(),
+        MetricName='NetworkPacketsIn',
+        Namespace='AWS/EC2',
+        Statistics=['Average'],
+        Dimensions=[{'Name':'InstanceId', 'Value':id}]
+        )
+        results.write(f"\nNetwork Packets Received for machine: {id}\n")
+        for datapoint in response['Datapoints']:
+            results.write(f"{datapoint['Timestamp'].astimezone()} --> {datapoint['Average']}\n")
+
+    results.write('-----------------------------------------------------------\n')
+
+    #NETWORK PACKETS SENT ON ALL CPUS
+
+    results.write('Network Packets Sent on all 9 instance:\n')
+    results.write('-----------------------------------------------------------\n')
+    results.write('Network Packets Sent on the instance from cluster1:\n')
+    for id in c1_ids:
+        response = cw_client.get_metric_statistics(
+        Period=60,
+        StartTime=datetime.utcnow() - timedelta(minutes=60),
+        EndTime=datetime.utcnow(),
+        MetricName='NetworkPacketsOut',
+        Namespace='AWS/EC2',
+        Statistics=['Average'],
+        Dimensions=[{'Name':'InstanceId', 'Value':id}]
+        )
+        results.write(f"\nNetwork Packets Sent for machine: {id}\n")
+        for datapoint in response['Datapoints']:
+            results.write(f"{datapoint['Timestamp'].astimezone()} --> {datapoint['Average']}\n")
+
+    results.write('\nNetwork Packets Sent on the instance from cluster2:\n')
+    for id in c2_ids:
+        response = cw_client.get_metric_statistics(
+        Period=60,
+        StartTime=datetime.utcnow() - timedelta(minutes=60),
+        EndTime=datetime.utcnow(),
+        MetricName='NetworkPacketsOut',
+        Namespace='AWS/EC2',
+        Statistics=['Average'],
+        Dimensions=[{'Name':'InstanceId', 'Value':id}]
+        )
+        results.write(f"\nNetwork Packets Sent for machine: {id}\n")
+        for datapoint in response['Datapoints']:
+            results.write(f"{datapoint['Timestamp'].astimezone()} --> {datapoint['Average']}\n")
+
+    results.write('-----------------------------------------------------------\n')
+
+    #REQUESTS ON CLUSTER 1 AND CLUSTER 2
+
+    results.write('Requests on both cluster:\n')
+    results.write('-----------------------------------------------------------\n')
+    response = cw_client.get_metric_statistics(
+    Period=60,
+    StartTime=datetime.utcnow() - timedelta(minutes=60),
+    EndTime=datetime.utcnow(),
+    MetricName='RequestCount',
+    Namespace='AWS/ApplicationELB',
+    Statistics=['Sum'],
+    Dimensions=[{'Name':'TargetGroup', 'Value':tgc1.get('TargetGroups')[0].get('TargetGroupArn')}]
+    )
+    results.write("\nRequests for cluster1: \n")
+    if len(response['Datapoints']) == 0:
+        results.write("None found\n")
+        print(response)
+    else:
+        for datapoint in response['Datapoints']:
+            results.write(f"{datapoint['Timestamp'].astimezone()} --> {datapoint['Sum']}\n")
+
+
+    response = cw_client.get_metric_statistics(
+    Period=60,
+    StartTime=datetime.utcnow() - timedelta(minutes=60),
+    EndTime=datetime.utcnow(),
+    MetricName='RequestCount',
+    Namespace='AWS/ApplicationELB',
+    Statistics=['Sum'],
+    Dimensions=[{'Name':'TargetGroup', 'Value':tgc2.get('TargetGroups')[0].get('TargetGroupArn')}]
+    )
+    results.write("\nRequests for cluster2: \n")
+    if len(response['Datapoints']) == 0:
+        results.write("None found\n")
+        print(response)
+    else:
+        for datapoint in response['Datapoints']:
+            results.write(f"{datapoint['Timestamp'].astimezone()} --> {datapoint['Sum']}\n")
+
+    results.write('-----------------------------------------------------------\n')
+
+
+    #REQUESTS PER TARGET ON CLUSTER 1 AND CLUSTER 2
+
+    results.write('Requests per target on both cluster:\n')
+    results.write('-----------------------------------------------------------\n')
+    response = cw_client.get_metric_statistics(
+    Period=60,
+    StartTime=datetime.utcnow() - timedelta(minutes=60),
+    EndTime=datetime.utcnow(),
+    MetricName='RequestCountPerTarget',
+    Namespace='AWS/ApplicationELB',
+    Statistics=['Sum'],
+    Dimensions=[{'Name':'TargetGroup', 'Value':tgc1.get('TargetGroups')[0].get('TargetGroupArn')}]
+    )
+    results.write("\nRequests per target for cluster1: \n")
+    if len(response['Datapoints']) == 0:
+        results.write("None found\n")
+        print(response)
+    else:
+        for datapoint in response['Datapoints']:
+            results.write(f"{datapoint['Timestamp'].astimezone()} --> {datapoint['Sum']}\n")
+
+
+    response = cw_client.get_metric_statistics(
+    Period=60,
+    StartTime=datetime.utcnow() - timedelta(minutes=60),
+    EndTime=datetime.utcnow(),
+    MetricName='RequestCountPerTarget',
+    Namespace='AWS/ApplicationELB',
+    Statistics=['Sum'],
+    Dimensions=[{'Name':'TargetGroup', 'Value':tgc2.get('TargetGroups')[0].get('TargetGroupArn')}]
+    )
+    results.write("\nRequests per target for cluster2: \n")
+    if len(response['Datapoints']) == 0:
+        results.write("None found\n")
+        print(response)
+    else:
+        for datapoint in response['Datapoints']:
+            results.write(f"{datapoint['Timestamp'].astimezone()} --> {datapoint['Sum']}\n")
+
+    results.write('-----------------------------------------------------------\n')
+
+    #TARGET RESPONSE TIME ON CLUSTER 1 AND CLUSTER 2
+
+    results.write('Target response time on both cluster:\n')
+    results.write('-----------------------------------------------------------\n')
+    response = cw_client.get_metric_statistics(
+    Period=60,
+    StartTime=datetime.utcnow() - timedelta(minutes=60),
+    EndTime=datetime.utcnow(),
+    MetricName='TargetResponseTime',
+    Namespace='AWS/ApplicationELB',
+    Statistics=['Average'],
+    Dimensions=[{'Name':'TargetGroup', 'Value':tgc1.get('TargetGroups')[0].get('TargetGroupArn')}]
+    )
+    results.write("\nTarget response time for cluster1: \n")
+    if len(response['Datapoints']) == 0:
+        results.write("None found\n")
+        print(response)
+    else:
+        for datapoint in response['Datapoints']:
+            results.write(f"{datapoint['Timestamp'].astimezone()} --> {datapoint['Average']}\n")
+
+
+    response = cw_client.get_metric_statistics(
+    Period=60,
+    StartTime=datetime.utcnow() - timedelta(minutes=60),
+    EndTime=datetime.utcnow(),
+    MetricName='TargetResponseTime',
+    Namespace='AWS/ApplicationELB',
+    Statistics=['Average'],
+    Dimensions=[{'Name':'TargetGroup', 'Value':tgc2.get('TargetGroups')[0].get('TargetGroupArn')}]
+    )
+    results.write("\nTarget response time for cluster2: \n")
+    if len(response['Datapoints']) == 0:
+        results.write("None found\n")
+        print(response)
+    else:
+        for datapoint in response['Datapoints']:
+            results.write(f"{datapoint['Timestamp'].astimezone()} --> {datapoint['Average']}\n")
+
+    results.write('-----------------------------------------------------------\n')
+
+    #SUCCESSFUL REQUESTS FOR CLUSTER 1 AND CLUSTER 2
+
+    results.write('Successful requests (2xx) on both clusters:\n')
+    results.write('-----------------------------------------------------------\n')
+    response = cw_client.get_metric_statistics(
+    Period=60,
+    StartTime=datetime.utcnow() - timedelta(minutes=60),
+    EndTime=datetime.utcnow(),
+    MetricName='HTTPCode_Target_2XX_Count',
+    Namespace='AWS/ApplicationELB',
+    Statistics=['Sum'],
+    Dimensions=[{'Name':'TargetGroup', 'Value':tgc1.get('TargetGroups')[0].get('TargetGroupArn')}]
+    )
+    results.write("\nSuccessful requests (2xx) for cluster1: \n")
+    if len(response['Datapoints']) == 0:
+        results.write("None found\n")
+        print(response)
+    else:
+        for datapoint in response['Datapoints']:
+            results.write(f"{datapoint['Timestamp'].astimezone()} --> {datapoint['Sum']}\n")
+
+
+    response = cw_client.get_metric_statistics(
+    Period=60,
+    StartTime=datetime.utcnow() - timedelta(minutes=60),
+    EndTime=datetime.utcnow(),
+    MetricName='HTTPCode_Target_2XX_Count',
+    Namespace='AWS/ApplicationELB',
+    Statistics=['Sum'],
+    Dimensions=[{'Name':'TargetGroup', 'Value':tgc2.get('TargetGroups')[0].get('TargetGroupArn')}]
+    )
+    results.write("\nSuccessful requests (2xx) for cluster2: \n")
+    if len(response['Datapoints']) == 0:
+        results.write("None found\n")
+        print(response)
+    else:
+        for datapoint in response['Datapoints']:
+            results.write(f"{datapoint['Timestamp'].astimezone()} --> {datapoint['Sum']}\n")
+
+    results.write('-----------------------------------------------------------\n')
--- Assignment 1/main.py
+++ Assignment 1/main.py
@@ -2,9 +2,10 @@ import time
 import boto3
 import requests
 import threading
-import datetime
+from datetime import datetime
 from instances import *
 from ELB import *
+from analysis import *
 
 
 def scenario1(elb_dns, path):
@@ -22,34 +23,34 @@ def scenario2(elb_dns, path):
 
 def benchmark(elb_dns):
     # Cluster 1 benchmark
-    now = datetime.datetime.now()
+    now = datetime.now()
     print(str(now) + " --> Starting benchmark for cluster 1")
     thread1 = threading.Thread(target=scenario1, args=(elb_dns, "/cluster1",))
     thread2 = threading.Thread(target=scenario2, args=(elb_dns, "/cluster1",))
     
-    now = datetime.datetime.now()
+    now = datetime.now()
     print(str(now) + " --> Starting both threads for cluster 1")
     thread1.start()
     thread2.start()
     thread1.join()
     thread2.join()
-    now = datetime.datetime.now()
+    now = datetime.now()
     print(str(now) + " --> Ending benchmark for cluster 1")
 
 
     # Cluster 2 benchmark
-    now = datetime.datetime.now()
+    now = datetime.now()
     print(str(now) + " --> Starting benchmark for cluster 2")
     thread1 = threading.Thread(target=scenario1, args=(elb_dns, "/cluster2",))
     thread2 = threading.Thread(target=scenario2, args=(elb_dns, "/cluster2",))
 
-    now = datetime.datetime.now()
+    now = datetime.now()
     print(str(now) + " --> Starting both threads for cluster 2")
     thread1.start()
     thread2.start()
     thread1.join()
     thread2.join()
-    now = datetime.datetime.now()
+    now = datetime.now()
     print(str(now) + " --> Ending benchmark for cluster 2")
 
 
@@ -58,9 +59,9 @@ def main():
     
     #initialise the attributs and clients that will be used
     #The follow 3 can change based on who wants to run (the access token is under me: Aleks)
-    aws_access_key_id = 'ASIAQJSMAGZDT52JX4MM'
-    secret_access_key = 'dVq3C7vYNcHs6gwsWfUREkU+YMAB8Mms1cQXUvJs'
-    session_token = 'FwoGZXIvYXdzED8aDLONnpYzhG7cywXQjSLEAS5IPHCLXfBUPX3BhWOw0bQ/BoBaSYDK4voOBvNG1bU9AF7Ra5niTOxP2WeZ1gMDWU3zFAfJM3IKUTXdARNI8OQYaFwyhXV+t3lmw0ZJKoA+KDqAi+rewK27CAI2ZyEAkNQSIk1pSZ7av5nGgo8VH7P9UL6Zlb5OpvlLLylYqxLLXzIJH/iFNJHq5beJht4k6EaureKn5LnZZbpP+8ZRVFL/yM8246JtGCLxceHgGrwTGB0roqHYOpSClZ6+D2dl8LmuOdwo3OmxmgYyLfZxCu66DT7R8bOOakkAA0eRPH97gJW79xE6rlJuFqr6GfzFeeWkneN23reerg=='
+    aws_access_key_id = 'ASIAQJSMAGZDQENEWDSC'
+    secret_access_key = 'X7vpvIMF4TfQb0VEqyp327NZeNqI/6vbfpyahc7N'
+    session_token = 'FwoGZXIvYXdzEEMaDHmkuDu2nuu9Wqy/NyLEAZHV0mp21d1TCdsICqtPsu2uRJEvEyHo6OGVmdWcz54KmXHo2HnZaUywhTzE/KVFUWBGA0k+0BYHU9iCz4Ti/o8OK51AmgsplfsTWp+16Ek/Tt2XCemlb1gqO//enJZB+W70pnpPkBkGZIi423n0DAkquncR8bczmTowEpZZ0Tvz7Q+80Wa5cFjMzPhhA/YKK6Xj1mt81oLvmorD8IvWiG3a9rcEWN9drPABPQv27Qo2ec93Z9BeH68D9GsNjKUklIa7glAowdSymgYyLctCqMOnM+TGRl0Q4Ko/wgxcIXMIpXxansEkF4rUcTaFRfH6jS26yq98B2eapg=='
     
     
     #Keypair for access and security group that needs to be assigned when instance is made (currently under me: Aleks)
@@ -83,6 +84,14 @@ def main():
     region_name= 'us-east-1'
     )
     
+    #Cloudwatch client
+    CW = boto3.client('cloudwatch',
+    aws_access_key_id=aws_access_key_id,
+    aws_secret_access_key=secret_access_key,
+    aws_session_token=session_token,
+    region_name= 'us-east-1'
+    )
+    
     #Create 5 m4.large instances
     m4Large_cluster_ids = create_m4large_cluster(EC2, keyPairName, securityGroup)
     #Create 4 t2.large instances
@@ -94,8 +103,8 @@ def main():
     print(t2Large_cluster_ids)
     
     # Give some breathing room for instances to initialise 
-    print("Sleeping for 5 min for instances to initialise and set up flask")
-    time.sleep(300)
+    print("Sleeping for 2 min for instances to initialise and set up flask")
+    time.sleep(120)
     
     #get security group id
     sg_response = EC2.describe_security_groups(
@@ -131,59 +140,65 @@ def main():
     #Register 5 m4.large instances to target group 1 and 4 t2.large instances to target group 2
     register_instances_to_target_groups(ELB, m4Large_cluster_ids, t2Large_cluster_ids, tg_cluster1, tg_cluster2 )
 
-    print("Give 5 min for ELB to get active")
-    time.sleep(300)
+    print("Give 3 min for ELB to get active")
+    time.sleep(180)
 
     #Benchmark
     benchmark(ELB_DNS)
-    
-    #Destroy everything when user write 'yes'
-    destructor(EC2, ELB, m4Large_cluster_ids, t2Large_cluster_ids, load_balancer, listener, tg_cluster1, tg_cluster2)
+
+    print("Take a 1 min breather post-requests before doing the analysis...")
+    time.sleep(60)
+
+    #cloudwatch metrics analysis
+    print("Running analysis...")
+    analysis(CW, m4Large_cluster_ids, t2Large_cluster_ids, tg_cluster1, tg_cluster2)
+
+    destroy = False
+    while destroy == False:
+        answer = input("- Enter 'terminate' to terminate the instance, elastic load balancer and target groups\n- Enter 'analysis' to pull metrics again\nChoice?:") 
+        if answer == "terminate":
+            #Destroy everything when user write 'yes'
+            destructor(EC2, ELB, m4Large_cluster_ids, t2Large_cluster_ids, load_balancer, listener, tg_cluster1, tg_cluster2)
+            destroy = True
+        elif answer == 'analysis':
+            analysis(CW, m4Large_cluster_ids, t2Large_cluster_ids, tg_cluster1, tg_cluster2)
+        else: 
+            continue 
             
-    
-    
-    
     
     
 def destructor(ec2_client, elb_client, m4Large_cluster_ids, t2Large_cluster_ids, load_balancer, listener, tg_cluster1, tg_cluster2):
-    destroy = False
-    while destroy == False:
-        answer = input("Enter 'yes' to terminate the instance, elastic load balancer and target groups: ") 
-        if answer == "yes":
             
-            print("Deleting the load balancer (in the same time deleting the listener and rules)...")
-            elb_client.delete_listener(ListenerArn=listener.get('Listeners')[0].get('ListenerArn'))
+    print("Deleting the load balancer (in the same time deleting the listener and rules)...")
+    elb_client.delete_listener(ListenerArn=listener.get('Listeners')[0].get('ListenerArn'))
+    
+    time.sleep(20) 
+    
+    elb_client.delete_load_balancer(
+        LoadBalancerArn = load_balancer.get('LoadBalancers')[0].get('LoadBalancerArn')
+    )
+    
+    time.sleep(20) 
+    
+    #deregister the instances
+    deregister_instances_from_target_groups(elb_client, m4Large_cluster_ids, t2Large_cluster_ids, tg_cluster1, tg_cluster2)
+    
+    time.sleep(10) 
+    
+    print("Deleting both target groups...")
+    elb_client.delete_target_group(
+        TargetGroupArn=tg_cluster1.get('TargetGroups')[0].get('TargetGroupArn')
+    )
+    elb_client.delete_target_group(
+        TargetGroupArn=tg_cluster2.get('TargetGroups')[0].get('TargetGroupArn')
+    )
+    
+    time.sleep(10) 
+    
+    print("Terminating instances...")
+    terminate_instance_cluster(ec2_client, m4Large_cluster_ids)
+    terminate_instance_cluster(ec2_client, t2Large_cluster_ids)
             
-            time.sleep(20) 
-            
-            elb_client.delete_load_balancer(
-                LoadBalancerArn = load_balancer.get('LoadBalancers')[0].get('LoadBalancerArn')
-            )
-            
-            time.sleep(20) 
-            
-            #deregister the instances
-            deregister_instances_from_target_groups(elb_client, m4Large_cluster_ids, t2Large_cluster_ids, tg_cluster1, tg_cluster2)
-            
-            time.sleep(10) 
-            
-            print("Deleting both target groups...")
-            elb_client.delete_target_group(
-                TargetGroupArn=tg_cluster1.get('TargetGroups')[0].get('TargetGroupArn')
-            )
-            elb_client.delete_target_group(
-                TargetGroupArn=tg_cluster2.get('TargetGroups')[0].get('TargetGroupArn')
-            )
-            
-            time.sleep(10) 
-            
-            print("Terminating instances...")
-            terminate_instance_cluster(ec2_client, m4Large_cluster_ids)
-            terminate_instance_cluster(ec2_client, t2Large_cluster_ids)
-            
-            destroy = True
-        else: 
-            continue 
 
      
 main()
