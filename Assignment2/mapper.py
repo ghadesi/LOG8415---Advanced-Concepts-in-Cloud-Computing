9794dbcffd0e32dc7897b3dda0c520a0ef9abcfe
Rezghool <80580733+Rezix93@users.noreply.github.com>
2022-10-30 16:56:03 -0400

    add comments
    comments for first part of the mapper.py

--- Assignment2/mapper.py
+++ Assignment2/mapper.py
@@ -8,12 +8,18 @@ https://www.michael-noll.com/tutorials/writing-an-hadoop-mapreduce-program-in-py
 import sys
 
 Data = {}
+#Takes input here
 lines = sys.stdin.readlines()
 
+#First convert file to a proper format list
+#The format in each line is this: (Persion ID)(TAB)(Friend,Friend,Friend,...)
+#So first of all we should split data by "/t" to have each person and his/her friend in a list so It's like : {'Person_ID': ['Friend_ID', 'Friend_ID',...],...}
+#I case if a person does not have any friend just put a empty value for person_ID key.
 for line in lines:
     data = line.replace("\n", "").split("\t")
     if data[1] == "" : Data.update({data[0]:[]})
-    else : Data.update({data[0]:data[1].split(",")})
+    else : Data.update({data[0]:data[1].split(",")}
+             
 for person,personsFriends in Data.items():
     sugestedFriends = []
     for eachFriend in personsFriends:
