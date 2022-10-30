ee937559fd70bd9b32260cd5f21bf9987654d353
Rezghool <80580733+Rezix93@users.noreply.github.com>
2022-10-30 18:51:50 -0400

    Update reducer.py
    complete comment for reducer

--- Assignment2/reducer.py
+++ Assignment2/reducer.py
@@ -10,7 +10,12 @@ from collections import Counter
 
 #From mapper we have: {'Person_ID': ['Friend_ID', 'Friend_ID',...],...}. Here we want to have most repeated elements for each person. (The first 10 ones as mention in homwork.)
 #First of all get line, split data by "\t" to have both person_id and friends.
-#The rest is magic of python. we have a 
+#The rest is magic of python.  Counter will give you the count of each of the elements in the tuple given. Also has a method call "most_common" to sort result base on key number of repetitions
+#First we use most_common(10) and give first 10 anwers and it is not kind of true because the value is not ascending sort wehn value is same.
+# In second try we first sort friends and then run .most_common(10). It worked well on our pc but not result is not sorted after map-reducer. We don't know why.
+#In our last try we just do Counter(friends).most_common() and get all result. Then sort it by sorted() function. 
+# Lambda functions are created, used, and immediately destroyed. sorted method sort base on (-x[1], x[0]). it means first elemtns with higher number of repetitions and then in same number of repetitions, lower number of user_id
+# Next select first 10 values of all sorted found sugested friends.
 maxFriend = 10
 for line in sys.stdin:
     line = line.strip()
